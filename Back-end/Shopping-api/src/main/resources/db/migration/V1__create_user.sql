9e6b0edeae1f83cd670a5bddd4e33a567fdd3eac
YOUR NAME HERE <ULLOA_SANTIAGO@YAHOO.COM>
2019-11-10 01:22:09 -0500

    edited db.migration file

--- Back-end/Shopping-api/src/main/resources/db/migration/V1__create_user.sql
+++ Back-end/Shopping-api/src/main/resources/db/migration/V1__create_user.sql
@@ -1,5 +1,15 @@
 CREATE TABLE users (
     id SERIAL,
-    user_name VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
     password VARCHAR(50)
+);
+
+CREATE TABLE todos (
+    id SERIAL,
+    name VARCHAR(100)
+);
+
+CREATE TABLE user_todolist (
+    user_id SERIAL,
+    todo_id SERIAL
 );
